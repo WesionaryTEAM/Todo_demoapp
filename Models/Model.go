d0e1196bcdf5916df4a3426da9550c74e82b4b29
Dnes <dinesh.iwacademy@gmail.com>
2020-08-06 13:06:01 +0545

    added more fields

--- Models/Model.go
+++ Models/Model.go
@@ -5,6 +5,7 @@ type Todo struct {
 	ID          uint   `json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
+	Rating      uint   `json:"rating"`
 }
 
 //TableName is ... explicitly set tablestruct
--- Models/todos.go
+++ Models/todos.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	config "Todo_demoapp/Todo_demoapp/Config"
+	"fmt"
 )
 
 //GetAllTodos is ...
@@ -9,5 +10,34 @@ func GetAllTodos(todo *[]Todo) (err error) {
 	if err = config.DB.Find(todo).Error; err != nil {
 		return err
 	}
+	return nil
+}
+
+//CreateATodo is ...
+func CreateATodo(todo *Todo) (err error) {
+	if err = config.DB.Create(todo).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+//GetATodo is ...
+func GetATodo(todo *Todo, id string) (err error) {
+	if err := config.DB.Where("id = ?", id).First(todo).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+//UpdateATodo is ...
+func UpdateATodo(todo *Todo, id string) (err error) {
+	fmt.Println(todo)
+	config.DB.Save(todo)
+	return nil
+}
+
+//DeleteATodo is ...
+func DeleteATodo(todo *Todo, id string) (err error) {
+	config.DB.Where("id = ?", id).Delete(todo)
 	return nil
 }
