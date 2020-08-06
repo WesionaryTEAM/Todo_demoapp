5ea488af9d96fe183e3f0d84a55e423a29a37ad8
Dnes <dinesh.iwacademy@gmail.com>
2020-08-06 13:05:21 +0545

    added update and delete Todo

--- Controllers/controller.go
+++ Controllers/controller.go
@@ -17,3 +17,56 @@ func GetTodos(c *gin.Context) {
 		c.JSON(http.StatusOK, todo)
 	}
 }
+
+//CreateATodo is ...
+func CreateATodo(c *gin.Context) {
+	var todo models.Todo
+	c.BindJSON(&todo)
+	err := models.CreateATodo(&todo)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, todo)
+	}
+}
+
+// GetATodo is ... via id field
+func GetATodo(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var todo models.Todo
+	err := models.GetATodo(&todo, id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, todo)
+	}
+}
+
+//UpdateATodo is ...
+func UpdateATodo(c *gin.Context) {
+	var todo models.Todo
+	id := c.Params.ByName("id")
+	err := models.GetATodo(&todo, id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, todo)
+	}
+	c.BindJSON(&todo)
+	err = models.UpdateATodo(&todo, id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, todo)
+	}
+}
+
+// DeleteATodo is ...
+func DeleteATodo(c *gin.Context) {
+	var todo models.Todo
+	id := c.Params.ByName("id")
+	err := models.DeleteATodo(&todo, id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, gin.H{"id:" + id: "deleted"})
+	}
+}
